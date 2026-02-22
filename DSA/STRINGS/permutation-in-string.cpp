b0affaa754b42ea368395ecb766074ca98f5ea8b
ABHISHEK SAXENA <a49549552@gmail.com>
2026-02-22 15:19:45 +0530

    permutation-in-string.cpp

---
+++ DSA/STRINGS/permutation-in-string.cpp
@@ -0,0 +1,50 @@
+/*
+========================================
+Problem: Permutation in String
+Platform: LeetCode
+Language: C++
+Approach: Fixed Sliding Window + Frequency Count
+Time Complexity: O(n)
+Space Complexity: O(1)
+========================================
+
+Example:
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: "ba" is a permutation of "ab"
+*/
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if (s1.length() > s2.length())
+            return false;
+
+        int count1[26] = {0};
+        int count2[26] = {0};
+
+        for (int i = 0; i < s1.length(); i++) {
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
+        }
+
+        if (equal(begin(count1), end(count1), begin(count2)))
+            return true;
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+
+            count2[s2[i] - 'a']++;
+            count2[s2[i - s1.length()] - 'a']--;
+
+            if (equal(begin(count1), end(count1), begin(count2)))
+                return true;
+        }
+
+        return false;
+    }
+};
